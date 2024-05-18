2dfb61cf1c90cc5f47b9fc20278306b5db5bd2e2
Ashley <ashjansen8@gmail.com>
2024-05-17 22:59:23 -0400

    add head function and eslint fix

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed : ${actual} === ${expected}. 👌👌👌`);
+  } else {
+    console.log(`'Assertion Failed: ${actual} !== ${expected}'`);
+  }
+};
+
+let head = function (arr1) {
+  return arr1[0]
+}
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
