ae1a7aa6f02feede8c2929a575672afaf1a6ffaf
Ashley <ashjansen8@gmail.com>
2024-05-17 22:26:41 -0400

    update function base on AI feedback

--- assertEqual.js
+++ assertEqual.js
@@ -3,10 +3,12 @@ const assertEqual = function(actual, expected) {
   const errorMsg = `'Assertion Failed: ${actual} !== ${expected}'`;
   if (actual === expected) {
     console.log(`Assertion Passed : ${actual} === ${expected}. 👌👌👌`);
-    console.assert(actual === expected, "%o", {errorMsg});
+  } else {
+    console.log(errorMsg);
   }
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual('Hey', 'Hey');
 assertEqual(1,3);
+//console.assert(actual === expected, "%o", {errorMsg}
