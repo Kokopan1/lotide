960250278ab772f3e7f5968039fce6629ece3de9
Ashley <ashjansen8@gmail.com>
2024-05-19 21:58:13 -0400

    fixed a condition to make it true, fixed the loop so it wouldnt return true after evaluating the first condition

--- eqArrays.js
+++ eqArrays.js
@@ -11,8 +11,6 @@ const eqArrays = function(arr1, arr2) {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         return false;
-      } else {
-        return true;
       }
     }
   } else {
@@ -26,5 +24,5 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 assertEqual(eqArrays([], []), false);
-assertEqual(eqArrays([1, 2], []), false);
+assertEqual(eqArrays([1, 2], []), true);
 assertEqual(eqArrays([1], [1]), true);
