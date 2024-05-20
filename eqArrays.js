b8ced3dc804fabda2d73d3a50d40db0d52b53aa2
Ashley <ashjansen8@gmail.com>
2024-05-19 22:47:54 -0400

    eslint the changes

--- eqArrays.js
+++ eqArrays.js
@@ -6,24 +6,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-/* const eqArrays = function(arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
         return false;
       }
-    }
-  } else {
-    return true;
-  }
-};*/
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false
-      } 
     } return true;
   } return false;
 };
