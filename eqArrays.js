b6075f95510772ae79407da5bf83749dc416740c
Ashley <ashjansen8@gmail.com>
2024-05-19 21:37:02 -0400

    fixed condition to see if the arrays are the same length, fixed to compare all array elements with a for loop instead of individal elements, this will now accomidate for any array lengths

--- eqArrays.js
+++ eqArrays.js
@@ -7,14 +7,21 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if (arr1[0] === arr2[0] && arr1[1] === arr2[1] && arr1[2] === arr2[2]) {
-    return true;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   } else {
     return false;
   }
+  
 };
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
