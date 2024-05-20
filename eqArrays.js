793c024fdc18cc2e5f5ed3f70b06781d0577e8ee
Ashley <ashjansen8@gmail.com>
2024-05-19 22:25:08 -0400

    fixed the scoping issue for my loop

--- eqArrays.js
+++ eqArrays.js
@@ -6,7 +6,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(arr1, arr2) {
+/* const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
@@ -16,11 +16,20 @@ const eqArrays = function(arr1, arr2) {
   } else {
     return true;
   }
-  
+};*/
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } 
+  } return true
 };
+
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 assertEqual(eqArrays([], []), true);
