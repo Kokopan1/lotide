ef16699a5b1313a60564001ef89b8c0014853c05
Ashley <ashjansen8@gmail.com>
2024-05-19 21:53:11 -0400

    fixed the looping condition to return false immediately if the arr[i]'s dont match each other, fixed 3 testing conditions

--- eqArrays.js
+++ eqArrays.js
@@ -9,10 +9,10 @@ const assertEqual = function(actual, expected) {
 const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        return true;
-      } else {
+      if (arr1[i] !== arr2[i]) {
         return false;
+      } else {
+        return true;
       }
     }
   } else {
@@ -23,8 +23,8 @@ const eqArrays = function(arr1, arr2) {
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays([],[]), true);
-assertEqual(eqArrays([1, 2], []), true);
-assertEqual(eqArrays([1], [1], true));
+assertEqual(eqArrays([], []), false);
+assertEqual(eqArrays([1, 2], []), false);
+assertEqual(eqArrays([1], [1]), true);
