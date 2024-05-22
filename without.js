a1cfb6d56d219e407995047d5e715dcdbde7b6bd
Ashley <ashjansen8@gmail.com>
2024-05-22 14:33:10 -0400

    add the test cases

--- without.js
+++ without.js
@@ -38,3 +38,8 @@ console.log(without([1, 2, 3], [1])); // => [2, 3]
 console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
 //how do i use the assetArraysEqual???
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
