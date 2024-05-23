6a02434fc1838e486e8fcfc7e3df253ff44de2a1
Ashley <ashjansen8@gmail.com>
2024-05-23 12:40:38 -0400

    pushed both the math.floor and math.ceil into middleNum in one step instead of creating new variables to contain each and then concating them

--- middle.js
+++ middle.js
@@ -19,23 +19,20 @@ const assertArraysEqual = function(expected, actual) {
 const middle = function(array) {
 
   let middleNum = [];
-  let middleLow = [];
-  let middleHi = [];
+
   //find if array length is under/equal to 2
   if (array.length <= 2) {
     return middleNum;
+
     //find if array length is odd
   } else if (array.length % 2 !== 0) {
     middleNum.push(array[Math.round((array.length - 1) / 2)]);
 
     //the array length is even
   } else {
-    //find the middle bottom of the arr length
-    middleLow.push(array[Math.floor((array.length - 1) / 2)]);
-    //find the middle top of the arr length
-    middleHi.push(array[Math.ceil((array.length - 1) / 2)]);
-    // add the two together
-    middleNum = middleLow.concat(middleHi);
+    
+    //find the middle bottom of the arr length, find the middle top of the arr length
+    middleNum.push(array[Math.floor((array.length - 1) / 2)], array[Math.ceil((array.length - 1) / 2)]);
   }
   return middleNum;
 };
