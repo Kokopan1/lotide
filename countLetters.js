0e464abac72b904370e8ccfe0728f1cd1cb69f8d
Ashley <ashjansen8@gmail.com>
2024-05-23 18:48:59 -0400

    create the function skeleton, added in the for of loop to access string elements

--- countLetters.js
+++ countLetters.js
@@ -7,16 +7,25 @@ const assertEqual = function(actual, expected) {
 };
 
 //For example, countLetters('LHL') should return results indicating that L appears twice, and H once.
-const countLetters = function (sentence) {
+
+const countLetters = function(strings) {
   let results = {}
-  for (const letters in sentence) {
-    if (results[letters]) {
-      results[letters] += 1;
-    } else {
-      results[letters] = 1;
-    }
+  for (const letter of strings) {
+    console.log(letter)
+    
   }
+  
   return results
 };
-
 console.log(countLetters('heeey there'))
+
+
+//Idea 1
+//could you also copy the string and set all the values as zero
+//if it doesnt match any properties in the object then push it into the object
+//if it does match the object, then increase the value of the key by 1
+
+//Idea 2
+//push the element as a kay-value pair into an object
+//if it doesnt match any properties in the object then push it into the object
+//if it does match the object, then increase the value of the key by 1
