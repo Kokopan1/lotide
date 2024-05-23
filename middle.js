479759457190b52c438be7ae9ca4023b336e3817
Ashley <ashjansen8@gmail.com>
2024-05-23 12:18:27 -0400

    wrote the function, still need to eslint and add test functions

--- middle.js
+++ middle.js
@@ -1,19 +1,24 @@
 const middle = function(array) {
 
-  let middleNum = []
+  let middleNum = [];
+  let middleLow = [];
+  let middleHi = [];
 
   if (array.length <= 2) {
     return middleNum
-  } else if (array.length%2) {
-    middleNum.push(array[Math.round((array.length) / 2)]);
-    middleNum.push(array[Math.round((array.length) / 2)]);
-    console.log(middleNum)
-  } else {
-  
+  } else if (array.length%2 != 0) {
     middleNum.push(array[Math.round((array.length - 1) / 2)]);
-    console.log(middleNum)
+    //console.log(middleNum)
+  } else {
+    middleLow.push(array[Math.floor((array.length - 1) / 2)]);
+    middleHi.push(array[Math.ceil((array.length - 1) / 2)]);
+    middleNum = middleLow.concat(middleHi);
   }
-  return middleNum
+  return middleNum;
 };
 
-console.log(middle([1,2,3,4,5,6]))
+console.log(middle([1,2,3,5,6]))
+console.log(middle([1]))
+console.log(middle([1, 2]))
+console.log(middle([1, 2, 3]))
+console.log(middle([1, 2, 3, 4]))
