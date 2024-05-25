49038ed424ea301b9c5c8c3211bdc14c407d43db
Ashley <ashjansen8@gmail.com>
2024-05-25 18:23:08 -0400

    map.js file input code from lesson, need to do tests after

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,15 @@ const assertEqual = function(actual, expected) {
     console.log(`'Assertion Failed: ${actual} !== ${expected}'`);
   }
 };
+
+//  ==== Test 1: Test Strings Failed ====
 assertEqual("Lighthouse Labs", "Bootcamp");
+
+//  ==== Test 2: Test Number Pass ====
 assertEqual(1, 1);
+
+//  ==== Test 3: Test Strings Pass ====
 assertEqual('Hey', 'Hey');
+
+//  ==== Test 4: Test Number Failed ====
 assertEqual(1,3);
---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } return true;
+  } return false;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed : ${actual} === ${expected}. 👌👌👌`);
+  } else {
+    console.log(`'Assertion Failed: ${actual} !== ${expected}'`);
+  }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+//                   (array, callback -> function called on e/ element in array)
+const results1 = map(words, (word) => word[0]);
+console.log(results1);
+
+// ==== Test 1: ====
+// ==== Test 2: ====
+// ==== Test 3: ====
