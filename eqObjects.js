aa6022289e86878ad32d837372f36f6b2989b0b8
Ashley <ashjansen8@gmail.com>
2024-05-26 00:11:50 -0400

    added assertEqual tests

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,13 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    } return true;
+  } return false;
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed : ${actual} === ${expected}. 👌👌👌`);
@@ -6,31 +16,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-//pseudo code
-
-/*
-first we want to loop through the object and find the number of keys the object has
-we want to do that for the second one as well
-when we have that we want to compare the number of keys to see if they match
-if they match when we want to compare that the values for For  THAT SAME KEY   in is the same as the other object
-
-*/
-
-/*
-From there we can compare both the array lengths. If they are not equal, we should stop the test and return false
-
-After that check, loop through the keys returned by Object.keys for one of the objects (not both). Use for..of since the keys are an array. Other loop types can work too, but this one is most elegant and idiomatic, thanks to ES6
-Inside our loop, compare both objects' values for that key. Use === to ensure that the types are the same!
-As soon as there is not a match, we can return false
-Our control flow will therefore only get to the end of the loop if all the keys matched. We can and should thus return true at the end (after the loop)
-
-*/
 
 // Check if the number of keys in both objects are the same
 const eqObjects = function (object1, object2) {
     let obj1Keys = Object.keys(object1)
     let obj2Keys = Object.keys(object2)
-   if (Object.keys(object1).length !== Object.keys(object2).length) {;
+   if (obj1Keys.length !== obj2Keys.length) {;
     return false;  //if key length is not the same, return false
    } else {
     for (const keys of obj1Keys) {  //iterates over keys in obj1Key to see if the key:value match object2
@@ -69,4 +60,7 @@ const longSleeveMultiColorShirtObject = {
   colors: ["red", "blue"],
   sleeveLength: "long",
 };
-eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject); // => false
+eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject); 
+
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject ), true);
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
