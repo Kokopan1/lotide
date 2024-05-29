7b98031e8f6630b492cbf5efc325aea8fa5a1037
Ashley <ashjansen8@gmail.com>
2024-05-29 16:56:18 -0400

    made my own example

--- findKey.js
+++ findKey.js
@@ -8,23 +8,25 @@ const assertEqual = function(actual, expected) {
 
 //Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
 const findKey = function(obj, callback) { 
-  for (const key in obj) {
-    console.log(key)
-  }
+  for (const key in obj) {  //looping through the keys in the object to get x
+    if (callback(obj[key])) {  // if the callback is true
+      return key  // return the key where callback is true
+   }
+  
+    }
+  return undefined; // else return undefined
 }
 
 
 
-
+console.log(
 findKey(
   {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
+    cat1: { furColor: 1 },
+    cat2: { furColor: 2 },
+    cat3: { furColor: 2 },
+    cat4: { curColor: 3 },
   },
-  (x) => x.stars === 2
-); 
-// => "noma"
+  (x) => x.furColor === 2
+)); 
+// => "cat2"
