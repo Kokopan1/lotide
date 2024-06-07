bb5d072ef70cf55c3f1adcd3e5879c415a0c4d1d
Ashley <ashjansen8@gmail.com>
2024-06-07 00:18:14 -0400

    removed code, made dry

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed : ${actual} === ${expected}. 👌👌👌`);
-  } else {
-    console.log(`'Assertion Failed: ${actual} !== ${expected}'`);
-  }
-};
+
+const assertEqual = require("./assertEqual");
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
@@ -16,10 +11,4 @@ const eqArrays = function(arr1, arr2) {
   } return false;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([1, 2], []), false);
-assertEqual(eqArrays([1], [1]), true);
+module.exports = eqArrays;
