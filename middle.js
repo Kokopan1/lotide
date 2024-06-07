7befbfcab144fc1dc5733e8ebc6d56574920f503
Ashley <ashjansen8@gmail.com>
2024-06-07 01:17:04 -0400

    made pretty, add imports

--- middle.js
+++ middle.js
@@ -1,21 +1,8 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    } return true;
-  } return false;
-};
+// Import Functions
+const assertArraysEqual = require("./assertArraysEqual");
+const eqArrays = require("./eqArrays");
 
-const assertArraysEqual = function(expected, actual) {
-  if (eqArrays(expected, actual)) {
-    console.log(`🟢 Assertion Passed : ${expected} === ${actual}.`);
-  } else {
-    console.log(`⛔ Assertion Failed: ${expected} !== ${actual}.`);
-  }
-};
-
+// Function Find Middle of Array
 const middle = function(array) {
 
   let middleNum = [];
@@ -37,25 +24,5 @@ const middle = function(array) {
   return middleNum;
 };
 
-// ==== Test Output ====
-console.log(middle([1,2,3,5,6]));
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4]));
-
-
-// ===== TEST CASE 1: Testing Number Array =====
-console.log(middle([1, 2, 3, 4])); // => [1, 2, 3, 4]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-
-// ==== TEST CASE 2: Testing String Array ====
-console.log(middle(['hey', 'there', 'you', 'good', 'looking']));
-assertArraysEqual(middle(['hey', 'there', 'you', 'good', 'looking']), ['you']);
-
-// ===== TEST CASE 3: testing array modification =====
-const testNumber = [1, 2, 6];
-
-const testNumberCopy = [...testNumber];//makes copy of test number (spread operator, similar to slice)
-console.log(middle(testNumberCopy));
-assertArraysEqual(testNumberCopy, testNumber); // we did not modify the OG array
+// Export Function
+module.exports = middle;
