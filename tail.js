dc6552780f8d34d31c741e8543a4698b8d4bffd0
Ashley <ashjansen8@gmail.com>
2024-06-07 00:11:29 -0400

    refactor

--- tail.js
+++ tail.js
@@ -1,13 +1,10 @@
 
-//const assertEqual = require("../assertEqual");
 const assertEqual = require("./assertEqual");
-
 
 
 const tail = function(arr1) {
   return arr1.slice(1);
 };
-
 
 
 module.exports = tail;
