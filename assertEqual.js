686d98ce13020e263c08347f9295b7763a9ac1ba
Ashley <ashjansen8@gmail.com>
2024-06-06 23:24:19 -0400

    removed code modularize it

--- assertEqual.js
+++ assertEqual.js
@@ -7,17 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-/*
-//  ==== Test 1: Test Strings Failed ====
-assertEqual("Lighthouse Labs", "Bootcamp");
-
-//  ==== Test 2: Test Number Pass ====
-assertEqual(1, 1);
-
-//  ==== Test 3: Test Strings Pass ====
-assertEqual('Hey', 'Hey');
-
-//  ==== Test 4: Test Number Failed ====
-assertEqual(1,3);
-*/
 module.exports = assertEqual;
