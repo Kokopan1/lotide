f9ebc4c3e7442b8815c6f3d50e77ec4974edadac
Ashley <ashjansen8@gmail.com>
2024-06-08 12:45:22 -0400

    changed to LF format

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,11 @@
 // Import Functions
+/*
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
-
+*/
 
 const assert = require('chai').assert;
-const head   = require('../tail');
+const tail = require('../tail');
 
 describe("#tail", () => {
   //Check for One Element
@@ -28,37 +29,8 @@ describe("#tail", () => {
   it("returns Labs for tail([\"Yo Yo\", \"Lighthouse\", \"Labs\"])[1]", () => {
     assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"])[1], "Labs");
   });
-})
+});
 
 
 
-// ==== Test Case 0: Checking Output
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are 
 
-
-// ==== Test Case 1: to see if an array with one element should yield an empty array for its tail ====
-
-const oneElement = ["One element"];
-const oneElementTail = tail(oneElement);
-console.log(oneElementTail);
-tail(oneElement);
-assertEqual(oneElementTail.length, 0);
-
-
-// ==== Test Case 2: to see if an empty array will yield an empty array for its tail
-const emptyArray = [];
-const emptyArrayTail = tail(emptyArray);
-console.log(emptyArrayTail);
-tail(emptyArray);
-assertEqual(emptyArrayTail.length, 0);
-
-
-// ==== Test Case 3: Check the returned array elements
-const result = tail(["Yo Yo", "Lighthouse", "Labs"]);
-tail(words);
-assertEqual(words.length, 3);
-
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
