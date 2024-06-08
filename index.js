ace25f5b188cd727832be722cf1ca610d695d0db
Ashley <ashjansen8@gmail.com>
2024-06-08 15:23:59 -0400

    added all the functions

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
-const head   = require('./head');
-const middle = require('./middle');
-const tail   = require('./tail');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 
 
 
 
 module.exports = {
-  head,
-  tail,
-  middle,
+  head:   head,
+  tail:   tail,
+  middle: middle,
   assertArraysEqual,
   assertEqual,
-  eqArrays
+  assertObjectsEqual,
+  countLetters,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
