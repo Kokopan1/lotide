7a4dc17d704be077dfca4bcaee6775206273d3fa
Ashley <ashjansen8@gmail.com>
2024-06-08 12:41:41 -0400

    wrote test cases

--- test/tailTest.js
+++ test/tailTest.js
@@ -3,6 +3,35 @@ const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
 
+const assert = require('chai').assert;
+const head   = require('../tail');
+
+describe("#tail", () => {
+  //Check for One Element
+  it("returns 0 for [\"One element\"]", () => {
+    assert.deepEqual(tail(["One element"]).length, 0);
+  });
+  //Check for Empty Array
+  it("returns 0 for []", () => {
+    assert.deepEqual(tail([]).length, 0);
+  });
+  //Check for Muilti Element Array
+  it("returns 2 for [\"Yo Yo\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2);
+  });
+
+  //Check Array is not Mutated
+  it("returns Lighthouse for tail([\"Yo Yo\", \"Lighthouse\", \"Labs\"])[0]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"])[0], "Lighthouse");
+  });
+
+  it("returns Labs for tail([\"Yo Yo\", \"Lighthouse\", \"Labs\"])[1]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"])[1], "Labs");
+  });
+})
+
+
+
 // ==== Test Case 0: Checking Output
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 console.log(tail(words)); // no need to capture the return value since we are 
