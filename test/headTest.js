8454d66bf3482c7caeafb8ed32a3e80da75a3299
Ashley <ashjansen8@gmail.com>
2024-06-08 12:55:35 -0400

    deleted old code

--- test/headTest.js
+++ test/headTest.js
@@ -1,17 +1,3 @@
-/*
-
-// Import Functions
-
-const assertEqual = require("../assertEqual");
-const head = require("../head");
-
-
-// ==== Test 1: Has Number ====
-assertEqual(head([5, 6, 7]), 5);
-
-// ==== Test 2: Has String ====
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-*/
 
 const assert = require('chai').assert;
 const head   = require('../head');
