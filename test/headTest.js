6cabe3eefd797515bcfbc3ae67d1a5f568867b04
Ashley <ashjansen8@gmail.com>
2024-06-07 22:28:58 -0400

    wrote test with M n C

--- test/headTest.js
+++ test/headTest.js
@@ -1,3 +1,5 @@
+/*
+
 // Import Functions
 
 const assertEqual = require("../assertEqual");
@@ -9,3 +11,27 @@ assertEqual(head([5, 6, 7]), 5);
 
 // ==== Test 2: Has String ====
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+*/
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello']), 'Hello'); 
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5]), 5); 
+  });
+});
+
+
